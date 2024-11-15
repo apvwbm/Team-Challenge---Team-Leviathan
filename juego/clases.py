42a09156da0af61504379c1c7c8418da59feac15
Jobave589 <jobave589@gmail.com>
2024-11-15 21:19:14 +0100

    cambios joaquin otra vez

--- juego/clases.py
+++ juego/clases.py
@@ -1,8 +1,9 @@
-class Tablero:
+class casa:
     def __init__(self, tamaño):
-        self.tamaño = tamaño
-    
-class Barco:
+        self.tamaño = joaquin
+
+
+class coche:
     def __init__(self, nombre, tamaño):
         self.nombre = nombre
-        self.tamaño = tamaño
+        self.tamaño = edad

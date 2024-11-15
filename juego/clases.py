798caadc9d08221a6be23a069a6cf6a1c764cd01
Aitor Pérez <aitorperezv12@gmail.com>
2024-11-15 21:02:43 +0100

    Creacion de archivos y cambios en Clases.py

---
+++ juego/clases.py
@@ -0,0 +1,8 @@
+class Tablero:
+    def __init__(self, tamaño):
+        self.tamaño = tamaño
+    
+class Barco:
+    def __init__(self, nombre, tamaño):
+        self.nombre = nombre
+        self.tamaño = tamaño
